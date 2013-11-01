33957677394c37f610373fc202e63780a0ccbe8a
Rachel Boyce <rfboyce@gmail.com>
2013-10-31 21:47:25 -0400

    updated db name to match remote

--- config/config.php
+++ config/config.php
@@ -14,7 +14,7 @@ If you want a different default time format for this app, set it below
 */
 
 # What is the name of this app?
-	define('APP_NAME', 'p2.edgeofforever.net');  
+	define('APP_NAME', 'PanoBlog');  
 
 # When email is sent out from the server, where should it come from?
 # Ideally, this should match the domain name
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'rfboyce@gmail.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', "p2_edgeofforever_net"); 
+	define('DB_NAME', "edgeoffo_p2_edgeofforever_net"); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
